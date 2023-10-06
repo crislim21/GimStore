4a4c19584feec0b4b9d0aa17005c6e2145604263
crisl <reavleaz@yahoo.co.id>
2023-10-06 14:46:30 +0700

    update link for image to display

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -181,7 +181,7 @@
                         <div class="col mb-5">
                             <div class="card h-100 ">
                                 <!-- Product image-->
-                                <img class="card-img-top img-fluid" src="{{ asset('/storage/'.$game->image) }}" style="height:170px;"
+                                <img class="card-img-top img-fluid" src="{{ asset('/storage'.$game->image) }}" style="height:170px;"
                                     alt="..." />
                                 <!-- Product details-->
                                 <div class="card-body p-4">
